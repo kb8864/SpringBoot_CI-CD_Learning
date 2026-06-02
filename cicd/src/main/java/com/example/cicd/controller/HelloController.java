c7b8cf8106ea0383c83bff8106f51560fce4e7dc
kb8864 <yt7s858864@gmail.com>
2026-06-02 12:32:13 +0900

    feat:静的解析の設定 以下のコマンドを入力して、全てでBUILD SUCCESSが表示されたことを確認。 確認できたことは、Checkstyleの静的解析が成功、自動テストが成功Mavenの検証フェーズまで成功、

--- cicd/src/main/java/com/example/cicd/controller/HelloController.java
+++ cicd/src/main/java/com/example/cicd/controller/HelloController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
 	@GetMapping("/hello")
-	
-	public Map<String, String> hello(){
+
+	public Map<String, String> hello() {
 		return Map.of(
 
 				"message", "GitHub Actions and ECS Fargate",
-
 				"timestamp", OffsetDateTime.now().toString()
 
 		);
--- cicd/src/test/java/com/example/cicd/controller/HelloControllerTest.java
+++ cicd/src/test/java/com/example/cicd/controller/HelloControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.cicd.controller;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +10,20 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HelloController.class)
- 	class HelloControllerTest {
-	
+class HelloControllerTest {
+
 	@Autowired
 
 	private MockMvc mockMvc;
-	
+
 	@Test
-	
+
 	void helloReturnsMessage() throws Exception {
 
 		mockMvc.perform(get("/api/hello"))
-		.andExpect(status().isOk())
-		.andExpect(content().contentTypeCompatibleWith("application/json"))
-		.andExpect(jsonPath("$.message", containsString("GitHub Actions and ECS Fargate")));
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith("application/json"))
+				.andExpect(jsonPath("$.message", containsString("GitHub Actions and ECS Fargate")));
 
 	}
 }
